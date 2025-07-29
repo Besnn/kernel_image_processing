0b4169658eebe928d900b4efd3ab5bcba3fbdcf8
Besnn <besniknuro74@gmail.com>
2025-07-29 17:37:55 +0200

    Total code rewrite. Changes: * PBM doesn't exist and won't be handled; * PGM/PGM will use image template with channels (1-4); * netPBM_io will handle reading anwriting   for images (1 & 4 channels); * nano is a very fucking annoying program;
    To do:
    * design and implement kernels;

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
 
-#include "src/codecs/PBMCodec.h"
-
 int main() {
-    auto pbm = new PBM(10, 10, P4);
-
-    delete pbm;
-    return 0;
 }
---
+++ src/netPBM_io.h
@@ -0,0 +1,8 @@
+//
+// Created by besnn on 7/29/25.
+//
+
+#ifndef NETPBM_IO_H
+#define NETPBM_IO_H
+
+#endif //NETPBM_IO_H

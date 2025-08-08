8f9725db8f7899f581447ddc911a13fd85701ace
besnn <besniknuro74@gmail.com>
2025-08-08 11:04:49 +0200

    added working image template with channels with move semantics

--- src/MultiChannelImage.h
+++ src/MultiChannelImage.h
@@ -8,6 +8,8 @@
 #include <map>
 #include <memory>
 #include <vector>
+#include <iostream>
+
 
 #include "types.h"
 
@@ -20,12 +22,28 @@ public:
     MultiChannelImage(u32 width, u32 height);
 private:
     u32 width, height;
-    std::map<std::string, Channel<T>> & channels;
+    std::map<std::string, Channel<T> * > channels;
+
+private:
+    ~MultiChannelImage();
 };
 
-//TODO: ???
-template<u8 channelNo, typename T>
-MultiChannelImage<channel_num, T>::MultiChannelImage(u32 width, u32 height) : width(width), height(height) {}
+
+template<u8 channel_num, typename T>
+MultiChannelImage<channel_num, T>::MultiChannelImage(u32 width, u32 height)
+: width(width), height(height)
+{
+    //TODO: see what's to be added
+    this->channels = std::map<std::string, Channel<T> * >();
+
+    auto RChannel = new Channel<T>(width * height);
+    auto GChannel = new Channel<T>(width * height);
+    auto BChannel = new Channel<T>(width * height);
+
+    this->channels.emplace("R", std::move(RChannel));
+    this->channels.emplace("G", std::move(GChannel));
+    this->channels.emplace("B", std::move(BChannel));
+}
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <unistd.h>
+#include "MultiChannelImage.h"
 
-int main() {
+
+int main()
+{
+    auto a = new MultiChannelImage<3, u8>(1000, 1000);
+
 }
